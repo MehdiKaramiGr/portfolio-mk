4dcc1a00fbd9a683de7ba550585b5d6327d9e417
mehdi <mehdi.grant@gmail.com>
2022-04-07 16:45:29 -0700

    header responsive & transition
    adding the functionality of the nav bar and adding a state to the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,40 @@ import React, { Component } from "react";
 import Logo from "../Logo/Logo";
 import "./Header.scss";
 
-
 export default class Header extends Component {
+	constructor() {
+		super();
+		this.state = {
+			navBarToggle: false,
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick = (e) => {
+		this.setState(
+			{ navBarToggle: !this.state.navBarToggle },
+			console.log(this.state.navBarToggle)
+		);
+	};
 	render() {
+		console.log(this.state.navBarToggle);
+		const theStyle = {transform: 'translateX(0%)'}
 		return (
 			<header className="header">
 				<Logo className="logo" />
+				<button className="mobile-nav-toggle" onClick={this.handleClick}>
+					<span className="sr-only">Menu</span>
+				</button>
 				<nav>
-					<ul className="primary-navigation">
+					<ul id="primary-navigation" style={this.state.navBarToggle?theStyle:null}>
 						<li className="active">
-							<a to="/home">Home</a>
+							<a href="#">
+								<span aria-hidden="true">01</span>Past Work
+							</a>
 						</li>
 						<li>
-							<a to="/about">About</a>
+							<a href="#">
+								<span aria-hidden="true">02</span>Skills
+							</a>
 						</li>
 					</ul>
 				</nav>
