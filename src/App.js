db5176450417e91ce5851a13d0b2a1577a52ac55
mehdi <mehdi.grant@gmail.com>
2022-04-07 09:09:04 -0700

    creating component structure and adding header/navBar to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.scss";
+import { Routes, Route, Link } from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+      <Header/>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
